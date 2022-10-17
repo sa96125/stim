7c2f9485f57a2631baf4d92288fa7279b79063e8
sa96125 <sa96125@gmail.com>
2022-10-18 08:50:55 +0900

    Set Firebase store

--- stim/stimApp.swift
+++ stim/stimApp.swift
@@ -6,15 +6,29 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct stimApp: App {
 
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    @StateObject private var authViewModel = AuthViewModel()
+    
     var body: some Scene {
         WindowGroup {
-            NavigationView {
-                ContentView()
-            }
+            ContentView().environmentObject(authViewModel)
         }
+    }
+}
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any]) async -> UIBackgroundFetchResult {
+        return .noData
     }
 }
