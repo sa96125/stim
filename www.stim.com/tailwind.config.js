2e0ed652fef2765a66c0f290d44dd8a11a4d524e
jspark <sa96125@texon.co.kr>
2024-03-22 14:24:08 +0900

    [chore] Update project dependencies

--- www.stim.com/tailwind.config.js
+++ www.stim.com/tailwind.config.js
@@ -1,13 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  prefix: "",
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./src/**/*.{ts,tsx}",
+    "./routes/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
   ],
-  prefix: "",
   theme: {
     container: {
       center: true,
