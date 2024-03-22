dbfdb868c71acaf2c90555258167c7d89cef32da
jspark <sa96125@texon.co.kr>
2024-03-22 11:28:43 +0900

    [chore] Update project dependencies

--- www.stim.com/vite.config.ts
+++ www.stim.com/vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from "vite-tsconfig-paths";
 import path from "path"
+import react from "@vitejs/plugin-react"
+import tsconfigPaths from "vite-tsconfig-paths"
+
+import { defineConfig } from "vite"
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin"
 
 const testConfig = {
   test: {
@@ -10,15 +12,15 @@ const testConfig = {
     setupFiles: "./src/tests/setup.ts",
     mockReset: true,
   },
-};
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths({})],
+  plugins: [react(), tsconfigPaths({}), TanStackRouterVite()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
   ...testConfig,
-});
+})
